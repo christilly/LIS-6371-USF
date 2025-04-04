e048660124beaf14e98f4d6f0152378e171426ac
christilly <156473204+christilly@users.noreply.github.com>
2025-04-04 12:23:17 -0400

    module 11

--- Module 8.R
+++ Module 8.R
@@ -11,7 +11,7 @@ library(boot)
 library(plyr)
 library(data.table)
 
-#Step 1: download Assignment 6 Dataset file 
+#Step 1: download Assignment 6 Dataset file
 studentData <- read.table("desktop/scripts/Assignment 6 Dataset.txt", header = TRUE, sep = ",")
 
 StudentAverage = ddply(studentData,"Sex",transform,Grade.Average=mean(Grade)) #transform makes new/modified columns in data frame
@@ -23,13 +23,12 @@ mean(sex) #returns NA because sex is composed of male and female as values
 write.table(StudentAverage, "Student_Average.csv", sep=",")
 
 
-#Step 2 Convert the data set to a dataframe for names whos' name contains the letter i, 
+#Step 2 Convert the data set to a dataframe for names whos' name contains the letter i,
 #then create a new data set with those names, Write those names to a file separated by comma’s (CSV)
 
 StudentNames = subset(studentData,grepl("[iI]", studentData$Name))
 StudentNames
 write.table(StudentNames,"NamesWithI.csv",sep=",")
-
 
 
 
